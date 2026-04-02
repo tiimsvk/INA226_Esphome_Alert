dac09f7560c1840b28d226ecbe606bb9ce213a42
TiiMsvk <52893640+tiimsvk@users.noreply.github.com>
2026-04-02 06:26:45 +0200

    Update output.py

--- components/mcp4725/output.py
+++ components/mcp4725/output.py
@@ -9,12 +9,18 @@ mcp4725 = cg.esphome_ns.namespace("mcp4725")
 MCP4725 = mcp4725.class_("MCP4725", output.FloatOutput, cg.Component, i2c.I2CDevice)
 
 CONF_SAVE_TO_EEPROM = "save_to_eeprom"
+CONF_SAVE_THRESHOLD = "save_threshold"
+CONF_SAVE_DEBOUNCE_MS = "save_debounce_ms"
+CONF_SAVE_MIN_INTERVAL_S = "save_min_interval_s"
 
 CONFIG_SCHEMA = (
     output.FLOAT_OUTPUT_SCHEMA.extend(
         {
             cv.Required(CONF_ID): cv.declare_id(MCP4725),
             cv.Optional(CONF_SAVE_TO_EEPROM, default=False): cv.boolean,
+            cv.Optional(CONF_SAVE_THRESHOLD, default=0.01): cv.percentage,  # relative 0..1
+            cv.Optional(CONF_SAVE_DEBOUNCE_MS, default=3000): cv.positive_time_period_milliseconds,
+            cv.Optional(CONF_SAVE_MIN_INTERVAL_S, default=60): cv.positive_time_period_seconds,
         }
     )
     .extend(cv.COMPONENT_SCHEMA)
@@ -28,5 +34,7 @@ async def to_code(config):
     await i2c.register_i2c_device(var, config)
     await output.register_output(var, config)
 
-    if config.get(CONF_SAVE_TO_EEPROM):
-        cg.add(var.set_save_to_eeprom(config[CONF_SAVE_TO_EEPROM]))
+    cg.add(var.set_save_to_eeprom(config[CONF_SAVE_TO_EEPROM]))
+    cg.add(var.set_save_threshold(config[CONF_SAVE_THRESHOLD]))
+    cg.add(var.set_save_debounce_ms(config[CONF_SAVE_DEBOUNCE_MS]))
+    cg.add(var.set_save_min_interval_s(config[CONF_SAVE_MIN_INTERVAL_S]))
